f4a5f4c00fa9685a765b720a0180d7a342a8276e
Tran Tri Vien <mr.betran.qn@gmail.com>
2022-01-05 21:58:40 +0700

    new commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/boxicons-2.1.1/css/boxicons.min.css'
+import Layout from './components/Layout'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
